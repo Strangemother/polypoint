6ea6e78078d20429df8ddb6ee7ca706ca88e4ef3
Jay Jagpal <jay@strangemother.com>
2025-02-10 19:58:40 +0000

    Missing file alert.

--- py_tools/run-pytool.py
+++ py_tools/run-pytool.py
@@ -1,4 +1,5 @@
-from tree import run_test
+
+import tree
 from tree.convert import Convert
 from pathlib import Path
 
@@ -7,7 +8,13 @@ doc_path = Path("C:/Users/jay/Documents/projects/polypoint/docs/")
 filepath = doc_path / "trees/ast-demo-js-tree/cut-cache/data-cut/_info.json"
 # filepath = doc_path / "trees/point-js-tree/cut-cache/data-cut/_info.json"
 output_dir = doc_path / "trees/clean/"
-run_test(tree_filepath=filepath, output_dir=output_dir)
+
+try:
+    tree.run_test(tree_filepath=filepath, output_dir=output_dir)
+except tree.MissingFile as err:
+    m = "Ensure the doc/tree-file exists, by parsing the acorn tree in the view."
+    print(f'\n\n\t -- {err}\n\t -- {m} -- \n\n')
+    raise err
 
 
 # Convert().flatten(filepath, output_dir)

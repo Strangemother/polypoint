6c791a184155227aa33096b1a5691d4232a293da
Jay Jagpal <jay@strangemother.com>
2025-02-16 04:39:03 +0000

    Locations, buttons, cleanup and styles.

--- theatre/planets.js
+++ theatre/planets.js
@@ -85,16 +85,26 @@ const planetColors = [
     { planet: "Neptune", color: "#2C4D97" },
     { planet: "Pluto", color: "#B2A393" }
 ];
-
-
-
  */
+
+const settings = {
+    zoom: .0006
+    // Pixel distance per planet
+    // if sun AU is not a factor
+    // , distanceMultiplier: 150
+
+    // if au is a factor
+    , distanceMultiplier: .16
+
+}
+
 const planets = {
     Sun: {
         color: "yellow",
+        // color: "yellow",
         diameter: 1_391_400
         , au: 0
-        , radius: 30
+        // , radius: 30
         // Each pixel is equal to 13,914KM
         // , radius: 100
         //      sun.diameter / sun.radius
@@ -142,19 +152,61 @@ const planets = {
         km: 2_872_500_000
     },
     Neptune: {
-        color: "#3C67DE",
+        // color: "#3C67DE",
+        color: "#aad7e8",
         au: 30.06,
         diameter: 49_528,
         km: 4_495_100_000
     },
     Pluto: {
-        color: "#C4A484",
-        au: 37,
-        diameter: 9_528
+            color: "#C4A484"
+        , au: 37
+        , diameter: 9_528
+        , km: 5_906_376_272
+        , max_km: 7_375_100_000
     }
 }
 
+locations = [[-546,470,834,270],
+             [411,117,85,270],
+             [610,373,72,270],
+             [592,155,30,270],
+             [512,587,29,270],
+             [370,390,7,270],
+             [436,329,7,270],
+             [832,584,5,270],
+             [434,437,4,270],
+             [384,462,2,270]]
 
+
+const addControls = function(){
+
+        addButton('load', {
+            onclick(ev) {
+                stage.points.fromLocalStore('planets');
+                stage.orderPlanets()
+            }
+        });
+
+        addButton('save', {
+            onclick(ev) {
+                stage.points.toLocalStore('planets');
+            }
+        });
+
+        addButton('restore', {
+            onclick(ev) {
+                stage.refreshPlanets()
+            }
+        });
+        addButton('delete', {
+            onclick(ev) {
+                delete localStorage.planets
+            }
+        });
+
+
+}
 
 class MainStage extends Stage {
     canvas='playspace'
@@ -162,12 +214,12 @@ class MainStage extends Stage {
     // live=false
     live = true
     mounted(){
-        this.multiplier = 1
+        this.multiplier = 150
         // this.point = new Point(50, 50)
         const r = 200
         const shareSize = 15
         this.near = this.center.copy()
-
+        addControls()
         this.generatePlanetList()
 
         // this.dis = new Distances
@@ -177,14 +229,55 @@ class MainStage extends Stage {
         // this.dis.onDragMove = this.onDragMove.bind(this)
         // this.dis.onDragEnd = this.onDragEnd.bind(this)
         this.rotSize = 0
-        this.perspectiveCenter = new Point({x:800, y:600})
+        // this.perspectiveCenter = new Point({x:800, y:600})
         this.spin = {
             x: -this.rotSize
             , y:  -98
             , z: -10
         }
 
-        this.dis.addPoints(...this.points, this.perspectiveCenter)
+        this.dis.addPoints(...this.points
+                // , this.perspectiveCenter
+                )
+
+        let sun = this.points.getByName('Sun')
+        this.g = (new Gradient).radial(sun)
+
+        let c1 = '#fbc148'
+        let c2 = '#d64b02'
+
+        this.g.addStops({
+            0: {color: c1}
+            , 1: {color: c2} // dark
+            // ,1: {color: '#c31a01'} // dark
+        })
+
+        this.moveSun(sun)
+        this.applySunGrad()
+
+        this.strokes.create('focul', {
+            dash: [5, 4]
+            , color: '#DDD'
+            , width: 2
+        })
+
+        let l = this.label = new Label(this.ctx, {
+            fontSize: 50
+            , text: "Planel Omega"
+            , textAlign: "center"
+            , fontName: '"lexend deca"'
+        })
+
+        l.fillStyle = 'orange'
+        l.position = new Point(300, 300)
+    }
+
+    moveSun(sun){
+        if(sun == undefined) {
+            sun = this.points.getByName('Sun')
+        }
+        sun.x = sun.radius * -.66
+        sun.y = this.center.y
     }
 
     generatePlanetList(){
@@ -192,19 +285,29 @@ class MainStage extends Stage {
         let origin = new Point({x: 100, y:100})
         let pl = Object.keys(planets).length
         this.points = PointList.generate.list(pl, offset, origin);
-        this.makePlanets(this.points, this.multiplier, 10)
+        this.makePlanets(this.points, settings.distanceMultiplier, 10)
+        this.orderPlanets()
+        this.points.fromUnpacked(locations)
+    }
 
-        // this.points.each(p=> {
-        //     // p.
-        // })
+    orderPlanets(){
+        /* Order the planets by size. Largest at the back. */
+        this.points.sort((a,b)=>b.radius-a.radius)
+    }
+
+    refreshPlanets() {
+        this.makePlanets(this.points, settings.distanceMultiplier, 10)
+        this.moveSun()
+        this.applySunGrad()
     }
 
     makePlanets(points, multiplier=2, offset=0) {
         /* Here we perform a _firstpass_ to add info to the points and planets.*/
         let ps = points || this.points;
+        let centerY = this.center.y
         /* install sizes and positions.*/
         let names = Object.keys(planets)
-        let sun = ps[0];
+        let sun = ps.getByName('Sun')
         let km = .000001//(sun.diameter / sun.radius) * .1;
 
         for (var i = 0; i < names.length; i++) {
@@ -213,26 +316,35 @@ class MainStage extends Stage {
             let info = planets[name]
             info.name = name;
             info.index = i
-
             let sunOffsetKM = i==0? 1: info.km * km
+            // info.x = (offset + (i * mul))
             info.x = (offset + sunOffsetKM * mul)
-            // info.radius = (info.diameter * .5)
+            info.y = centerY
+            info.radius = (info.diameter * settings.zoom)
             ps[i].update(info)
         }
     }
 
     draw(ctx){
-        this.spin.y += .5;
-        this.clear(ctx)
+        this.clear(ctx, '#000')
         // arcLine(ctx, this.points)
         // bisectAll(this.points)
         // ctx.stroke();
         this.drawCircles(ctx)
-        this.perspectiveCenter.pen.fill(ctx, '#880000')
-        // this.drawIris(ctx)
+        this.drawIris(ctx)
     }
 
-    drawCircles(ctx) {
+    applySunGrad(){
+        let sun = this.points.getByName('Sun')
+        this.g.radial() // refresh hack.
+        let grad = this.g.getObject(this.ctx)
+        sun.color = grad
+        // this.perspectiveCenter.pen.fill(ctx, '#880000')
+
+    }
+
+    draw3DCircles(ctx) {
+        this.spin.y += .5;
 
         const projectionPoint = this.points[0]
         const perspectiveCenter = this.perspectiveCenter
@@ -249,11 +361,23 @@ class MainStage extends Stage {
         // this.points.pen.fill(ctx)
     }
 
+    drawCircles(ctx) {
+        this.points.pen.fill(ctx)
+    }
+
     drawIris(ctx) {
         /* The dynamic highlighter. */
         let p = this.dis.getPoint();
+
         if(p) {
-            p.pen.circle(ctx)
+            this.label.text = p.name
+            let un = this.strokes.focul()
+            p.pen.circle(ctx, p.radius + 15, 'white', 2)
+            un()
+            this.label.writeText(ctx)
+            // p.pen.circle(ctx, undefined, 'white', 2)
+        } else {
+
         }
     }
 

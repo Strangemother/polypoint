30c73d05db2e18a7dc2b6313455b49eaf0928fc2
Jay Jagpal <jay@strangemother.com>
2025-03-11 01:33:02 +0000

    Add markdown info to file view

--- site/beta/documentation/views.py
+++ site/beta/documentation/views.py
@@ -2,6 +2,7 @@ from trim import views
 from django.conf import settings
 from pathlib import Path
 
+from examples import theatre
 
 
 def get_docs_list(sub_path=None, **kw):
@@ -90,21 +91,117 @@ class FileView(views.TemplateView):
         r['filepath'] = p
         r['exists'] = p.exists()
 
+        return self.add_file_info(r, p, **kwargs)
+
+    def add_file_info(self, r, p, **kwargs):
+        """coupled with the response_class, render the file disovered markdown
+        """
+        root = settings.POLYPOINT_DOCS_DIR
+        filepath = root / p
+        keyname = 'markdown_object'
+        r[keyname] = { "exists": False, "path": filepath.relative_to(root) }
+
+        if not filepath.exists():
+            return r
+
+        md_text = filepath.read_text()
+        r[keyname].update({
+            "exists": True,
+            "text": md_text,
+        })
+
+        parent = settings.POLYPOINT_DOCS_DIR
+        rel_path = filepath.relative_to(parent)
+        meta = theatre.render_markdown(rel_path, parent)
+        # meta = theatre.get_metadata(rel_path, parent, ensure_suffix=None)
+
+        r['file_exists'] = meta['filepath_exists']
+
+        # Generate an internal renderer.
+        response_kwargs = {} # kwargs
+        response_kwargs.setdefault("content_type", self.content_type)
+        md_response = SubMarkdownTemplateResponse(
+            request=self.request,
+            template=self.get_template_names(),
+            context=r,
+            using=self.template_engine,
+            **response_kwargs,
+        )
+
+        r['metadata'] = r[keyname]['meta'] = meta
+
+        md = meta.get('markdown', None)
+        if md:
+            r['markdown'] = md
+            r[keyname]['rendered'] = r['markdown']['html']
+        else:
+            print('no markdown object.')
         return r
 
 
-class DirView(views.TemplateView):
-    """Primary index page for the docs/ URL.
-    Present a list of all available files.
+from docs.views import SubMarkdownTemplateResponse
+
+class DirView(views.ListView):
+    """
     """
     template_name = 'documentation/dir.html'
 
+    def get_queryset(self):
+        """
+        Return the list of items for this view.
+        """
+        path = self.kwargs.get('path')
+        return get_docs_list(path)
+
     def get_context_data(self, **kwargs):
         r = super().get_context_data(**kwargs)
-        path = self.kwargs.get('path', '')
-        p = Path(path)
-        if p.exists() is False:
-            p = p.with_suffix('.md')
+        path = self.kwargs.get('path')
+        p = Path(path) # .with_suffix('.js')
         r['object_path'] = p
 
+        return self.add_readme_info(r, p, **kwargs)
+
+    def add_readme_info(self, r, p, **kwargs):
+        """coupled with the response_class, render the file disovered markdown
+        """
+        root = settings.POLYPOINT_DOCS_DIR
+        readme_path = root / p / 'readme.md'
+
+        r['readme'] = { "exists": False, "path": readme_path.relative_to(root) }
+
+        if not readme_path.exists():
+            return r
+
+        readme_md_text = readme_path.read_text()
+        r['readme'].update({
+            "exists": True,
+            "text": readme_md_text,
+        })
+
+        parent = settings.POLYPOINT_DOCS_DIR
+        rel_path = readme_path.relative_to(parent)
+        meta = theatre.render_markdown(rel_path, parent)
+        # meta = theatre.get_metadata(rel_path, parent, ensure_suffix=None)
+
+        r['file_exists'] = meta['filepath_exists']
+
+        # Generate an internal renderer.
+        response_kwargs = kwargs
+        response_kwargs.setdefault("content_type", self.content_type)
+        md_response = SubMarkdownTemplateResponse(
+            request=self.request,
+            template=self.get_template_names(),
+            context=r,
+            using=self.template_engine,
+            **response_kwargs,
+        )
+
+        r['readme']['meta'] = meta
+
+        md = meta.get('markdown', None)
+        if md:
+            r['markdown'] = md
+            r['readme']['rendered'] = r['markdown']['html']
+        else:
+            print('no markdown object.')
         return r

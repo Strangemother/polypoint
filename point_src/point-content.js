d9a429a71f8cc38931bd0e68d024eed4821d545a
Jay Jagpal <jay@strangemother.com>
2025-03-11 01:28:06 +0000

    Move into compass.js

--- point_src/point-content.js
+++ point_src/point-content.js
@@ -1,92 +1,13 @@
+/*
+files: compass.js
+ */
 Math.PI2 = Math.PI * 2
 
 
-const RIGHT = 0
-const DOWN = Math.PI*.5;
-const LEFT = Math.PI * 1;
-const UP = Math.PI*1.5;
 
-const RIGHT_DEG = radiansToDegrees(0);
-const DOWN_DEG = radiansToDegrees(Math.PI *.5);
-const LEFT_DEG = radiansToDegrees(Math.PI  * 1);
-const UP_DEG = radiansToDegrees(Math.PI *1.5);
-
-
-class Compass {
-
-    constructor(setup={}) {
-        this.func = setup.func
-    }
-
-    setDeg() {
-        this.func = radiansToDegrees
-    }
-
-    setRad() {
-        this.func = undefined;
-    }
-
-    static degrees() {
-        return new Compass({
-            func: radiansToDegrees
-        })
-    }
-
-    static get rad() {
-        return new Compass()
-    }
-
-    get(v) {
-        return this.conv(v)
-    }
-
-    get right(){
-         return this.conv(0)
-    }
-
-    get down(){
-         return this.conv(Math.PI *.5)
-    }
-
-    get left(){
-         return this.conv(Math.PI * 1)
-    }
-
-    get up(){
-         return this.conv(Math.PI * 1.5)
-    }
-
-    conv(v) {
-        if(this.func) {
-            return this.func(v)
-        }
-        return v
-    }
-}
-
-
-function radiansToDegrees(radians) {
-    return radians * (180 / Math.PI);
-}
-
-const TAU = 2 * Math.PI;
-const radiansToTau = function(radians) {
-    /*
-        let radians = Math.PI; // 1/2 TAU
-        console.log(radiansToTau(radians)); // Outputs: 0.5
-    */
-    return radians / TAU;
-}
-
-
-const degToRad = function(value) {
-    return value * (Math.PI / 180);
-}
-
-
-const isFunction = function(value) {
-    return (typeof(value) == 'function')
-}
+// const isFunction = function(value) {
+//     return (typeof(value) == 'function')
+// }
 
 // --
 /* PolyGen v3.1 | MIT License | git.io/fjq8z */
@@ -233,8 +154,6 @@ function projectFrom(origin, distance=undefined, rotation=undefined) {
 
     return { x, y };
 }
-
-
 
 
 class XAngle extends Number {

e549ae5de320df822fa254ef5d23558c03eccdc5
Jay <github@strangemother.com>
2025-10-11 17:34:56 +0000

    Clean up Gunicorn configuration files by removing unnecessary comments and whitespace

--- deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py
+++ deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py
@@ -1,12 +1,8 @@
-# From the server, in the deployment directory
-sudo cp /home/site/apps/polypoint/deployment/polypoint-deploy/deploy/gunicorn/start.sh \
-    /home/site/apps/polypoint/deploy/gunicorn/start.sh
-
-sudo cp /home/site/apps/polypoint/deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py \
-    /home/site/apps/polypoint/deploy/gunicorn/polypointjs_com_gunicorn.conf.pybind = "unix:/run/gunicorn/polypointjs.sock"
+bind = "unix:/run/gunicorn/polypointjs.sock"
 workers = 3
 user = "site"
 group = "site"
+umask = 0o007
 timeout = 30
 accesslog = "/var/log/gunicorn/polypointjs_com_access.log"
 errorlog = "/var/log/gunicorn/polypointjs_com_error.log"

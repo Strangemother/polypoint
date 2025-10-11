2e37482e2e8af38cf2b6a435fdc151e6b7f753de
Jay <github@strangemother.com>
2025-10-11 00:48:13 +0000

    Update Gunicorn configuration and start script for improved deployment

--- deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py
+++ deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py
@@ -1,4 +1,9 @@
-bind = "unix:/run/gunicorn/polypointjs.sock"
+# From the server, in the deployment directory
+sudo cp /home/site/apps/polypoint/deployment/polypoint-deploy/deploy/gunicorn/start.sh \
+    /home/site/apps/polypoint/deploy/gunicorn/start.sh
+
+sudo cp /home/site/apps/polypoint/deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py \
+    /home/site/apps/polypoint/deploy/gunicorn/polypointjs_com_gunicorn.conf.pybind = "unix:/run/gunicorn/polypointjs.sock"
 workers = 3
 user = "www-data"
 group = "www-data"
--- deployment/polypoint-deploy/deploy/gunicorn/start.sh
+++ deployment/polypoint-deploy/deploy/gunicorn/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Navigate to the Django project directory
-cd /home/site/apps/polypoint/site/beta
-
 # Activate the virtual environment
 source /home/site/apps/polypoint/.venv/bin/activate
 
+# Change to the Django project directory
+cd /home/site/apps/polypoint/site/beta || exit 1
+
+# Get the absolute path to the config file
+CONFIG_PATH="/home/site/apps/polypoint/deploy/gunicorn/$1"
+
 # Start the Gunicorn server using the config file
-exec gunicorn -c "$1" polypoint_beta.wsgi:application
+exec gunicorn -c "$CONFIG_PATH" polypoint_beta.wsgi:application

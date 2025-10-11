d26eac12ea7ff38495cb76f2304f5880563c3fa3
Jay <github@strangemother.com>
2025-10-11 17:05:53 +0000

    Refactor Gunicorn service configuration and update user/group settings; remove unused Django project files

--- deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py
+++ deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py
@@ -5,8 +5,8 @@ sudo cp /home/site/apps/polypoint/deployment/polypoint-deploy/deploy/gunicorn/st
 sudo cp /home/site/apps/polypoint/deployment/polypoint-deploy/deploy/gunicorn/polypointjs_com_gunicorn.conf.py \
     /home/site/apps/polypoint/deploy/gunicorn/polypointjs_com_gunicorn.conf.pybind = "unix:/run/gunicorn/polypointjs.sock"
 workers = 3
-user = "www-data"
-group = "www-data"
+user = "site"
+group = "site"
 timeout = 30
 accesslog = "/var/log/gunicorn/polypointjs_com_access.log"
 errorlog = "/var/log/gunicorn/polypointjs_com_error.log"

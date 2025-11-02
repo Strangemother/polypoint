3ebfdf7ea67b99bd51dcb2468b1c012d17365d77
Jay Jagpal <jay@strangemother.com>
2025-11-02 09:07:25 +0000

    site demos settings const

--- site/beta/primary/settings/base.py
+++ site/beta/primary/settings/base.py
@@ -24,7 +24,8 @@ from pathlib import Path
 
 SITE_DIR = (Path(__file__).parent / '../../../../').resolve().absolute()
 POLYPOINT_SRC_DIR = SITE_DIR / 'point_src'
-POLYPOINT_DEMO_DIR = SITE_DIR / 'site_demos'
+POLYPOINT_DEMO_DIR = SITE_DIR / 'demos'
+POLYPOINT_SITE_DEMO_DIR = SITE_DIR / 'site_demos'
 POLYPOINT_DOCS_DIR = SITE_DIR / 'docs'
 POLYPOINT_THEATRE_DIR = SITE_DIR / 'theatre'
 POLYPOINT_EXAMPLES_DIR = SITE_DIR / 'examples'

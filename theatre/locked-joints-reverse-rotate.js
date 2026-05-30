0b39fa3623beab4a45cabf73b462ccdfff491076
Jay Jagpal <jay@strangemother.com>
2026-05-30 05:12:54 +0100

    lock reverse concept

--- theatre/locked-joints-reverse-rotate.js
+++ theatre/locked-joints-reverse-rotate.js
@@ -17,31 +17,31 @@ files:
     ../point_src/constrain-distance.js
 ---
 
-A point cannot exceed the rotation around a point to a maxmium angle value,
-relative to the origin point zero.
+Same as locked-joint but when you move the knee joint, the hip rotates.
+Assigning a `resist` of `0` (default: `1`), allows the parent point to
+be rotated around its center.
 
-Meaning if we have a point (origin), and another point (knee). The position
-of the knee may not lye outside a cone projected from the center of origin,
-
-Rotating the origin will slide the position around the origin center
-Moving the knee will _lock_ at the edges of the projected cone.
-
-distance is handled by another element, and is optional here.
+If the resist value was `.5`, it would rotate
+50% of the pull distance of the target knee, and the knee reciprocates with 50%
+damping on its position.
  */
 class MainStage extends Stage {
     canvas='playspace'
 
     mounted(){
 
-        let a = this.a = new Point({x:200,y:200, radius: 50, rotation: 10, cone: 50})
+        let a = this.a = new Point({x:200,y:200, radius: 50,
+                            rotation: 10,
+                            cone: 50
+                        })
         let b = this.b = new Point({x:400,y:200, radius: 30,
                 rotation: 0,
                 /* degrees  either side of rotation point.
                 resulting in 10 arc degress allowed movement.*/
                 cone: 10})
-        let c = this.c = new Point({x:600,y:200, radius: 20})
+        // let c = this.c = new Point({x:600,y:200, radius: 20})
 
-        this.dragging.add(a, b, c)
+        this.dragging.add(a, b)
 
     }
 
@@ -50,11 +50,11 @@ class MainStage extends Stage {
 
         // this.a.rotation += 1
 
-        this.a.constraint.cone(this.b, this.a.cone)
+        this.a.constraint.cone(this.b, this.a.cone, { resist: 0})
         this.b.constraint.cone(this.c, this.b.cone)
 
         this.b.constraint.track(this.a, 200)
-        this.c.constraint.track(this.b, 100)
+        // this.c.constraint.track(this.b, 100)
 
         this.a.pen.indicator(ctx, {color:'#ddd'})
         this.b.pen.indicator(ctx, {color:'green'})

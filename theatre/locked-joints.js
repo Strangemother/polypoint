a8fc0de84de0b61f479ab78b2ce9c153944eb131
Jay Jagpal <jay@strangemother.com>
2026-06-02 01:00:23 +0100

    up dimensio

--- theatre/locked-joints.js
+++ theatre/locked-joints.js
@@ -14,6 +14,7 @@ files:
     ../point_src/dragging.js
     ../point_src/functions/clamp.js
     ../point_src/stage.js
+    ../point_src/cone.js
     ../point_src/constrain-distance.js
 ---
 
@@ -41,14 +42,15 @@ class MainStage extends Stage {
 
     mounted(){
 
-        let a = this.a = new Point({x:200,y:200, radius: 50, rotation: 10, cone: 50})
+        let a = this.a = new Point({x:200,y:200, radius: 50, rotation: 10, coneDeg: 50})
         let b = this.b = new Point({x:400,y:200, radius: 30,
                 rotation: 0,
                 /* degrees  either side of rotation point.
                 resulting in 10 arc degress allowed movement.*/
-                cone: 10})
+                coneDeg: 10})
         let c = this.c = new Point({x:600,y:200, radius: 20})
 
+        this.a.cone.update({ container: this.dimensions, innerOffset: 1, innerCurve: 1 })
         this.dragging.add(a, b, c)
 
     }
@@ -59,17 +61,18 @@ class MainStage extends Stage {
         // this.a.rotation += 1
 
         /* First clamp each child into its parent's allowed angular cone. */
-        this.a.constraint.cone(this.b, this.a.cone)
-        this.b.constraint.cone(this.c, this.b.cone)
+        this.a.constraint.cone(this.b, this.a.coneDeg)
+        this.b.constraint.cone(this.c, this.b.coneDeg)
 
         /* Then enforce the segment lengths. This preserves the locked angle
         while keeping each point on its radius from the parent. */
         this.b.constraint.track(this.a, 200)
         this.c.constraint.track(this.b, 100)
-
+        this.a.cone.fill(ctx, {color: '#010101'})
         this.a.pen.indicator(ctx, {color:'#ddd'})
         this.b.pen.indicator(ctx, {color:'green'})
         this.c.pen.indicator(ctx)
+
     }
 }
 
---
+++ theatre/updimension.js
@@ -0,0 +1,61 @@
+/*
+title: Up Dimension
+categories: minimal
+files:
+    head
+    point
+    pointlist
+    ../point_src/events.js
+    mouse
+    stage
+    dragging
+    stroke
+    ../point_src/random.js
+    ../point_src/rectangle.js
+---
+
+Up Dimension converts the 2d Plot into a 3D plot
+using the radius as a camera distance
+*/
+
+addButton('Plot', {
+    onclick(){
+        stage.plot()
+    }
+})
+
+addButton('shuffle', {
+    onclick(){
+        stage.shuffle()
+    }
+})
+
+class MainStage extends Stage {
+    canvas = 'playspace'
+    mounted() {
+        this.plot()
+    }
+
+    plot(){
+        this.points = PointList.generate.grid(48, 8, 100)
+        this.points.each.radius = ()=> random.int(5,15)
+        // random.shuffle(this.points, 2)
+        this.dragging.set(...this.points)
+    }
+
+    shuffle(){
+        random.shuffle(this.points, 2)
+    }
+
+    draw(ctx){
+        this.clear(ctx)
+
+        this.points.pen.fill(ctx, {color:'purple'})
+
+    }
+}
+
+// Polypoint.head.deferredProp('PointList', )
+
+stage = MainStage.go(/*{ loop: true }*/)
+

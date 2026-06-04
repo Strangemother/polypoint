646f1934bbdd24e086be4399d0427c06d7ae187b
Jay <github@strangemother.com>
2026-06-04 20:30:55 +0000

    Refactor arc functionality: update title to 'Semi Circle', enhance midpoint calculation for semi-circles, and adjust point handling in MainStage class.

--- theatre/arc-auto-three-point.js
+++ theatre/arc-auto-three-point.js
@@ -1,6 +1,6 @@
 /*
 ---
-title: Arc Three Point.
+title: Semi Circle.
 categories:
     arc
     angles
@@ -21,12 +21,43 @@ files:
     ../point_src/protractor.js
 ---
 
-Draw an arc to another point _through_ a third point.
+Draw a semi-circle to another point _through_ a third point.
 
 */
 
 // aa = new Angle(20, 'tau')
 // ab = new Angle(20).tau
+const getSemiMidpoint = function(fromPoint, toPoint, centerPoint, radius=.5) {
+    /* Return the center point x/y to ensure a perfect arc through the three points */
+    const dx = toPoint.x - fromPoint.x
+    const dy = toPoint.y - fromPoint.y
+    const distance = Math.hypot(dx, dy)
+
+    if(distance === 0) {
+        return {
+            x: fromPoint.x
+            , y: fromPoint.y
+        }
+    }
+
+    const midpointX = (fromPoint.x + toPoint.x) * .5
+    const midpointY = (fromPoint.y + toPoint.y) * .5
+    const _radius = distance * radius
+
+    // Keep the point on the same side of the chord while forcing a 180-degree arc.
+    const unitPerpX = -dy / distance
+    const unitPerpY = dx / distance
+    let direction = -1
+    if(centerPoint) {
+        const side = dx * (centerPoint.y - midpointY) - dy * (centerPoint.x - midpointX)
+        direction = side < 0 ? -1 : 1
+    }
+
+    return {
+        x: midpointX + (unitPerpX * _radius * direction)
+        , y: midpointY + (unitPerpY * _radius * direction)
+    }
+}
 
 
 class MainStage extends Stage {
@@ -36,7 +67,7 @@ class MainStage extends Stage {
         this.centerPoint = new Point({x:200, y:150, radius: 20, color: '#666'})
         this.fromPoint = new Point({x:100, y:300})
         this.toPoint = new Point({x:350, y:150})
-        this.dragging.addPoints(this.centerPoint, this.fromPoint, this.toPoint)
+        this.dragging.addPoints(this.fromPoint, this.toPoint)
     }
 
     draw(ctx){
@@ -45,6 +76,9 @@ class MainStage extends Stage {
         ctx.fillStyle = '#555'
         ctx.strokeStyle = 'orange'
 
+        const newCenter = getSemiMidpoint(this.fromPoint, this.toPoint, this.centerPoint)
+        this.centerPoint.update(newCenter)
+        
         this.centerPoint.pen.circle(ctx, undefined, '#555')
         // this.centerPoint.pen.line(ctx, undefined, 'red')
         this.fromPoint.pen.indicator(ctx, {color: 'red'})

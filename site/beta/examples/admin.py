370e6c05804f3582ef67a914e4d510d04ab05a1b
Jay <github@strangemother.com>
2026-06-29 22:07:53 +0000

    Implement TheatreFile admin interface with enhanced display and actions

--- site/beta/examples/admin.py
+++ site/beta/examples/admin.py
@@ -1,8 +1,72 @@
 from django.contrib import admin
 
-# Register your models here.
-from trim.admin import register_models
-
 from . import models
 
-register_models(models)
+
+@admin.register(models.TheatreFile)
+class TheatreFileAdmin(admin.ModelAdmin):
+    list_display = (
+        'filepath',
+        'name',
+        'still_image_compatible',
+        'has_still_image',
+        'modified',
+        'updated',
+    )
+    list_filter = (
+        'still_image_compatible',
+        'modified',
+        'updated',
+    )
+    search_fields = (
+        'filepath',
+        'name',
+        'description',
+        'crc',
+        'still_image_path',
+    )
+    ordering = ('filepath',)
+    list_per_page = 100
+    readonly_fields = (
+        'created',
+        'updated',
+    )
+    actions = (
+        'mark_still_image_compatible',
+        'mark_still_image_incompatible',
+    )
+    fieldsets = (
+        ('File', {
+            'fields': (
+                'filepath',
+                'name',
+                'description',
+            ),
+        }),
+        ('Image', {
+            'fields': (
+                'still_image_compatible',
+                'still_image_path',
+            ),
+        }),
+        ('Tracking', {
+            'fields': (
+                'crc',
+                'modified',
+                'created',
+                'updated',
+            ),
+        }),
+    )
+
+    @admin.display(boolean=True, description='Has still image')
+    def has_still_image(self, obj):
+        return bool((obj.still_image_path or '').strip())
+
+    @admin.action(description='Mark selected files as still image compatible')
+    def mark_still_image_compatible(self, request, queryset):
+        queryset.update(still_image_compatible=True)
+
+    @admin.action(description='Mark selected files as still image incompatible')
+    def mark_still_image_incompatible(self, request, queryset):
+        queryset.update(still_image_compatible=False)
